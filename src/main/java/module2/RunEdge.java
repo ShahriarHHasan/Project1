8daba0e9e1ad07d33694042431e566f653bb23a7
ShahriarHHasan <shantoneel1203@gmail.com>
2024-02-18 11:39:10 +0600

    Project setup and Browser launch

---
+++ src/main/java/module2/RunEdge.java
@@ -0,0 +1,11 @@
+package module2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class RunEdge {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+
+    }
+}
---
+++ src/main/java/module2/RunFirefox.java
@@ -0,0 +1,11 @@
+package module2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class RunFirefox {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+
+    }
+}
