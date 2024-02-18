94e767aa4a189ba27311ab82e3fe47b569c6b844
ShahriarHHasan <shantoneel1203@gmail.com>
2024-02-18 11:49:33 +0600

    add comment

--- src/main/java/module2/RunEdge.java
+++ src/main/java/module2/RunEdge.java
@@ -6,6 +6,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class RunEdge {
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver();
-
+    //open edge browser
     }
 }
